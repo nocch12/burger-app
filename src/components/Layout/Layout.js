b07149a12a1364c73eabe3b56e7a238e3594f6f5
nocch12 <k.ohno12.gt@gmail.com>
2020-09-04 23:51:17 +0900

    バーガーの仮表示まで

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Auxi from "../../hoc/Auxi";
+import classes from "./Layout.module.css";
+
+const layout = (props) => {
+  return (
+    <Auxi>
+      <div>Toolber, SideDrawer</div>
+      <main className={classes.Content}>{props.children}</main>
+    </Auxi>
+  );
+};
+
+export default layout;
