d51f3e206dc0eb14e023b1fa917e7220b3d294d8
nocch12 <k.ohno12.gt@gmail.com>
2020-09-05 16:40:58 +0900

    BuildControls View作成

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react';
 
 import Auxi from '../../hoc/Auxi';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {...}
+  // }
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 1,
+      cheese: 0,
+      meat: 1,
+    },
+  }
+
   render() {
     return (
       <Auxi>
-        <Burger />
-        <div>Buid Controls</div>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls />
       </Auxi>
     );
   }
