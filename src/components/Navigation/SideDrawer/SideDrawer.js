74d60968fbb8f30e572b8ac5602fdbe6370597b0
nocch12 <k.ohno12.gt@gmail.com>
2020-09-06 00:48:39 +0900

    一旦完成

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,7 +3,7 @@ import React from "react";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import Backdrop from "../../UI/Backdrop/Backdrop";
-import Auxi from "../../../hoc/Auxi";
+import Auxi from "../../../hoc/Auxi/Auxi";
 
 import classes from "./SideDrawer.module.css";
 
