cd409d6315e9f841834972a4c67859424b2ecc22
nocch12 <k.ohno12.gt@gmail.com>
2020-09-22 16:16:36 +0900

    環境変数での切り替え

--- src/index.js
+++ src/index.js
@@ -3,25 +3,29 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
-import thunk from 'redux-thunk';
+import thunk from "redux-thunk";
 
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import burgerBuilderReducer from "./store/reducers/burgerBuilder";
-import orderReducer from './store/reducers/order';
-import authReducer from './store/reducers/auth';
+import orderReducer from "./store/reducers/order";
+import authReducer from "./store/reducers/auth";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
   order: orderReducer,
-  auth: authReducer
-})
+  auth: authReducer,
+});
 
-const store = createStore(rootReducer, composeEnhancers(
-  applyMiddleware(thunk),
-));
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 const app = (
   <Provider store={store}>
